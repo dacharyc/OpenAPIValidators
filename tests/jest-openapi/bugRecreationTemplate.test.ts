ee984e46a6e0d980afc30b8afdac5474a8b55d48
dacharyc <dachary.carey@mongodb.com>
2025-08-19 17:58:04 -0400

    Restructure WIP - files moved, related updates still needed

---
+++ tests/jest-openapi/bugRecreationTemplate.test.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { inspect } from 'util';
+
+import jestOpenAPI from '../../packages/jest-openapi';
+
+const dirContainingApiSpec = path.resolve(
+  '../../commonTestResources/exampleOpenApiFiles/valid/bugRecreationTemplate',
+);
+
+describe('recreate bug (issue #XX)', () => {
+  beforeAll(() => {
+    const pathToApiSpec = path.join(dirContainingApiSpec, 'openapi.yml');
+    jestOpenAPI(pathToApiSpec);
+  });
+
+  const res = {
+    status: 200,
+    req: {
+      method: 'GET',
+      path: '/recreate/bug',
+    },
+    body: {
+      expectedProperty1: 'foo',
+    },
+  };
+
+  it('passes', () => {
+    expect(res).toSatisfyApiSpec();
+  });
+
+  it('fails when using .not', () => {
+    const assertion = () => expect(res).not.toSatisfyApiSpec();
+    expect(assertion).toThrow(
+      inspect({
+        body: {
+          expectedProperty1: 'foo',
+        },
+      }),
+    );
+  });
+});
